6d746cf42ba7ed203137584f34bd0b8f05bd61ec
Asitha Navaratne <asithan476@gmail.com>
2021-09-29 19:19:34 +0530

    Justified comments

--- main.c
+++ main.c
@@ -22,8 +22,8 @@ int main(void){
  */
 
 void PORT_INIT(void){
-	DDRB &= ~((1<<PB4)|(1<<PB5)|(1<<PB7));		///< Set MOSI, SCK and SS pins to input.
-	DDRB |= (1<<PB6);								///< Set MISO pin to output.
+	DDRB &= ~((1<<PB4)|(1<<PB5)|(1<<PB7));			///< Set MOSI, SCK and SS pins to input.
+	DDRB |= (1<<PB6);					///< Set MISO pin to output.
 }
 
 /*!
@@ -31,7 +31,7 @@ void PORT_INIT(void){
  */
 
 void SPI_SLAVE_INIT(void){
-	SPCR = (1<<SPE);								///< Enable SPI in Slave SPI mode.
+	SPCR = (1<<SPE);					///< Enable SPI in Slave SPI mode.
 }
 
 /*!
@@ -41,7 +41,7 @@ void SPI_SLAVE_INIT(void){
  */
 
 char SPI_DATA_EXCHANGE(char databyte){
-	SPDR = databyte;								///< Load the data byte to the SPDR shift register to transmit.
-	while(!(SPSR & (1<<SPIF)));					///< Wait until the full data byte is received.
-	return SPDR;									///< Return the received databyte.
+	SPDR = databyte;					///< Load the data byte to the SPDR shift register to transmit.
+	while(!(SPSR & (1<<SPIF)));				///< Wait until the full data byte is received.
+	return SPDR;						///< Return the received databyte.
 }
